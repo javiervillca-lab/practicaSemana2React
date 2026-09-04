23515078f8b31a1a63e7f560807d352b7b761048
Javier Villca Queso <xavi.villca@gmail.com>
2026-09-04 10:30:48 -0400

    se completo interconectar mi frontend con mi backend para el LOGIN, tambien se instalo Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 import Login from './components/Login'
 
@@ -10,9 +7,7 @@ function App() {
 
   return (
     <>
-      <div className="app-container">
-        <Login />
-      </div>
+      <Login />
     </>
   )
 }
